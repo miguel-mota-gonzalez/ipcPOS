e48db2a42139b436ae1e0be837aaf8847f550c5b
JUAN M MOTA GONZALEZ <mike.mota@gmail.com>
2022-05-24 21:33:50 -0500

    Add files via upload

---
+++ limpiarTablasEspejo.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import pymysql
+#import datetime
+from datetime import datetime
+from datetime import timedelta, date
+
+def ejecutarSentencia(querySQL, params):
+
+    # MySQL Server credentials
+    db = pymysql.connect("45.55.248.209","root","F3rn4nd0M","Invoices" )
+    cursor = db.cursor()
+
+    rowsCount=cursor.execute(querySQL, params)
+
+    db.commit()
+    db.close()
+
+    print("Registros actualizados {0}".format(rowsCount))
+
+    return rowsCount
+
+if __name__ == '__main__':
+
+    print("**** Limpiar tablas espejo ***")
+
+    params=[]	
+    ejecutarSentencia("call spIPCLimpiarTablasEspejo", params)	
+
